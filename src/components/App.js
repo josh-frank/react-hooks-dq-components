1a4d00488f0b0f211acdcf152869348d45fc922e
Josh Frank <eurosewusa@gmail.com>
2021-02-22 09:51:14 -0500

    Cam's a wizard from Brooklyn

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import SideMenu from "./SideMenu";
+import MainContent from "./MainContent";
+import TopMenu from "./TopMenu";
 
 function App() {
-  return <div className="app-container">{/* What components go here? */}</div>;
+  return (
+    <div className="app-container">
+      <SideMenu />
+      <MainContent />
+      <TopMenu />
+    </div>
+  );
 }
 
 export default App;
